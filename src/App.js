4eb4debf0ae82b06e95a70bb6cf6356655fb6957
modeal-chan <chanchan2@1-mm.net>
2020-09-14 09:55:26 +0900

    완료

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Menu from './components/Menu';
 import Home from './pages/Home';
@@ -15,18 +15,18 @@ import GoTop from './components/GoTop'
 function App() {
   return (
     <div className="container">
-      <Router>
+      <BrowserRouter>
         <Header/>
         <Menu/>
         <Switch>
-          <Route path="/home" exact={true} component={Home}/>
+          <Route path="/" exact={true} component={Home}/>
           <Route path="/promotion" component={Promotion}/>
           <Route path="/magazine" component={Magazine}/>
           <Route path="/quotation" component={Quotation}/>
           <Route path="/services" component={Services}/>
           <Route path="/profile" component={Profile}/>
-        </Switch>
-      </Router>
+          </Switch>
+      </BrowserRouter>
       <GoTop/>
     </div>
   );
